450d8d796df26678b2e080e3b8d54f00ed8d2fbd
shuang <shuang.lixx@qq.com>
2023-06-08 06:50:22 +0800

    完成copy功能

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,6 +8,7 @@ const Demo = () => {
   })
   const [allArticle, setAllArticle] = useState([])
   const [getSummary,{error, isFetching}] = useLazyGetSummaryQuery();
+  const [copied, setCopied] = useState('')
 
   useEffect(()=>{
     const articlesFromLocalStorage = JSON.parse(
@@ -31,6 +32,11 @@ const Demo = () => {
       console.log(newArticle)
       localStorage.setItem('articles',JSON.stringify(updatedAllArticle))
     }
+  }
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(()=> setCopied(false),3000)
   }
   return (
     <section className="mt-16 w-full max-w-xl">
@@ -71,8 +77,10 @@ const Demo = () => {
               onClick={()=>setArticle(item)}
               className='link_card'
             >
-            <div className='copy_btn'>
-              <img src={copy} alt="copy" className='w-[40%] h-[40%] object-contain' />
+            <div className='copy_btn' onClick={()=> handleCopy(item.url)}>
+              <img 
+              
+              src={copied === item.url ? tick : copy} alt="copy" className='w-[40%] h-[40%] object-contain' />
             </div>
             <p className=' flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
               {item.url}
