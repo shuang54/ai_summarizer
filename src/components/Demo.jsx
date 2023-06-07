ac3a9c48ce0cf8e1d436615f66558f2a07c2492e
shuang <shuang.lixx@qq.com>
2023-06-08 06:43:12 +0800

    显示历史记录，和加载动画，显示article

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,8 +6,17 @@ const Demo = () => {
     url:'',
     summary:'',
   })
-  const [allArticle, setAllArticle] = useState([]);
+  const [allArticle, setAllArticle] = useState([])
   const [getSummary,{error, isFetching}] = useLazyGetSummaryQuery();
+
+  useEffect(()=>{
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem('articles')
+    )
+    if(articlesFromLocalStorage){
+        setAllArticle(articlesFromLocalStorage)
+      }
+  },[])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,8 +24,12 @@ const Demo = () => {
     const { data } = await getSummary({ articleUrl:article.url})
     if(data?.summary){
       const newArticle = { ...article,summary:data.summary}
+      const updatedAllArticle = [newArticle,...allArticle]
+      
       setArticle(newArticle)
+      setAllArticle(updatedAllArticle)
       console.log(newArticle)
+      localStorage.setItem('articles',JSON.stringify(updatedAllArticle))
     }
   }
   return (
@@ -51,9 +64,50 @@ const Demo = () => {
         </form>
 
         {/* Browse URL History */}
+        <div className=' flex flex-col gap-1 max-h-60 overflow-y-auto'>
+          {allArticle.map((item,index)=>(
+            <div
+              key={`link-${index}`}
+              onClick={()=>setArticle(item)}
+              className='link_card'
+            >
+            <div className='copy_btn'>
+              <img src={copy} alt="copy" className='w-[40%] h-[40%] object-contain' />
+            </div>
+            <p className=' flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+              {item.url}
+            </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
+      <div className='my-10 max-w-full flex justify-center items-center'>
+        {isFetching ? (
+          <img src={loader} alt='loader' className=' w-20 h-20 object-contain' />
+        ):error ? (
+          <p className=' font-inter font-bold text-black text-center'>
+            Well, that wasn't supposed to happpen...
+            <br />
+            <span className='font-satoshi font-normal'>
+              {error?.data?.error}
+            </span>
+          </p>
+        ) : (
+          article.summary && (
+            <div className='flex flex-col gap-3'>
+              <h2 className='font-satoshi font-bold'>
+                Article <span className='blue_gradient'>Summary</span>
+              </h2>
+              <div className='summary_box'>
+                <p className='font-inter font-medium text-sm text-gray-700'>{article.summary}</p>
+              </div>
+            </div>
+          )
+        )}
+
+      </div>
     </section>
   )
 }
