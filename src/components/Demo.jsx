1c3d3d5719428548fec64379fcbe6648f36cabde
shuang <shuang.lixx@qq.com>
2023-06-08 06:14:32 +0800

    设置redux，可以发送请求获取数据

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,12 +1,23 @@
 import { useState,useEffect} from 'react'
 import {copy,linkIcon,loader,tick} from '../assets'
+import { useLazyGetSummaryQuery } from '../services/article'
 const Demo = () => {
   const [article, setArticle] = useState({
     url:'',
     summary:'',
   })
+  const [allArticle, setAllArticle] = useState([]);
+  const [getSummary,{error, isFetching}] = useLazyGetSummaryQuery();
+
   const handleSubmit = async (e) => {
-    alert('Submitted');
+    e.preventDefault();
+    
+    const { data } = await getSummary({ articleUrl:article.url})
+    if(data?.summary){
+      const newArticle = { ...article,summary:data.summary}
+      setArticle(newArticle)
+      console.log(newArticle)
+    }
   }
   return (
     <section className="mt-16 w-full max-w-xl">
